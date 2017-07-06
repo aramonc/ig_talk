ffd18205fc5c2bd2bf5bf7bd8428967584a35d65
Adrian Cardenas <arcardenas@gmail.com>
2017-07-05 23:20:58 -0400

    Add user record provider

---
+++ code/src/Provider.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Arc\IgTalk;
+
+use MongoDB\Client;
+use MongoDB\Collection;
+use MongoDB\Model\BSONDocument;
+
+class Provider
+{
+    /**
+     * @var Collection
+     */
+    private $collection;
+
+    public function __construct(Client $mongoClient)
+    {
+        $this->collection = $mongoClient->selectDatabase('igtalk')
+                                        ->selectCollection('users');
+    }
+
+    public function getPagedList(int $page = 0, int $limit = 100): array
+    {
+        $cursor = $this->collection->find(
+            [],
+            [
+                'limit' => $limit,
+                'skip' => $page * $limit,
+            ]
+        );
+
+        $users = [];
+
+        foreach ($cursor as $data) {
+            /** @var $data BSONDocument */
+            $users[] = $this->createUserEntity($data->getArrayCopy());
+        }
+
+        return $users;
+    }
+
+    protected function createUserEntity(array $data): User
+    {
+        $user = new User();
+        $user->_id = $data['_id'];
+        $user->firstName = $data['firstName'];
+        $user->lastName = $data['lastName'];
+        $user->phoneNumber = $data['phoneNumber'];
+        $user->password = $data['password'];
+        $user->email = $data['email'];
+
+        return $user;
+    }
+}
