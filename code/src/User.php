37d1badd5b26e35ed80519d254662ed2d5634076
Adrian Cardenas <arcardenas@gmail.com>
2017-07-05 21:58:28 -0400

    Add sample entity

---
+++ code/src/User.php
@@ -0,0 +1,17 @@
+<?php
+namespace Arc\IgTalk;
+
+class User
+{
+    public $_id;
+
+    public $firstName;
+
+    public $lastName;
+
+    public $email;
+
+    public $password;
+
+    public $phoneNumber;
+}
