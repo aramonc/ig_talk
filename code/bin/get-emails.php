ce2dc3a97933839d86286cb6ccd49b415bdda5df
Adrian Cardenas <arcardenas@gmail.com>
2017-07-05 23:21:13 -0400

    Add email lister

---
+++ code/bin/get-emails.php
@@ -0,0 +1,55 @@
+<?php
+
+use Arc\IgTalk\Provider;
+use Arc\IgTalk\User;
+use MongoDB\Client;
+use Monolog\Handler\ErrorLogHandler;
+use Monolog\Logger;
+use function \Arc\IgTalk\toHumanReadable;
+
+require_once __DIR__ . "/../vendor/autoload.php";
+
+$logger = new Logger('log');
+$logger->pushHandler(new ErrorLogHandler());
+
+$user = new User();
+
+$startTime = microtime(true);
+$baseMemory = memory_get_usage();
+
+$logger->addInfo(
+    'Starts with',
+    ['time' => $startTime, 'memory' => toHumanReadable($baseMemory)]
+);
+
+$provider = new Provider(new Client("mongodb://datastore:27017"));
+$emailLogPath = __DIR__ . '/../data/user.txt';
+$emailLog = fopen($emailLogPath, 'w+');
+
+$logger->addInfo(
+    'Pre-process:',
+    ['time' => $startTime, 'memory' => toHumanReadable($baseMemory)]
+);
+
+for ($up = 0; $up < 10000; $up++) {
+    $uc = 0;
+    foreach ($provider->getPagedList($up) as $user) {
+        /** @var $user User */
+        fwrite($emailLog, $user->email . "\n");
+
+        if ($uc % 500 === 0) {
+            $consumedM = memory_get_usage() - $baseMemory;
+            $consumedT = microtime(true) - $startTime;
+
+            $logger->addInfo(
+                'So far: ',
+                ['time' => $consumedT, 'memory' => toHumanReadable($consumedM)]
+            );
+        }
+
+        $uc++;
+    }
+}
+
+fclose($emailLog);
+unlink($emailLogPath);
